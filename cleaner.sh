1467c5f50037bb35aa0e4d4866e212667e17db8c
Liam <kingliam28@ntlworld.com>
2016-07-30 12:52:00 +0100

    User input for extensions to sort.

--- cleaner.sh
+++ cleaner.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
 # Cleans up desktop. Sorts files by extensions and stores them in temporary files.
 
-if [ -d "temporary_files" ] 
-	then 
-		echo "temporary_files directory already exists."
-	else 
-		echo "Creating temporary_files directory."
-		mkdir ~/desktop/temporary_files
+echo "What file extensions would you like to move clean up? (e.g. txt png mov)"
+read ext_variable
+
+if [ -d "temporary_files" ]
+	then
+	echo "temporary_files directory already exists."
+else
+	echo "Creating temporary_files directory."
+	mkdir ~/desktop/temporary_files
 fi
 
-for ext in txt mov png jpg html psd pdf xlsx zip m4a m4v
+for ext in $ext_variable
 do
 	if [ -d temporary_files/temp_$ext ]
-	then 
+		then
 		echo temp_$ext "already exists"
 	else
 		echo "Creating" temp_$ext "directory"
@@ -22,10 +25,11 @@ do
 	count=`ls -1 *.$ext 2>/dev/null | wc -l`
 
 	if [ $count != 0 ]
-	then 
+		then
 		echo "Moving all" $ext "files."
 		mv ~/desktop/*.$ext ~/desktop/temporary_files/temp_$ext
-	else 
-		echo "No files with this extension exists: " $ext
+	else
+		echo "No files with this extension exists: "$ext
 	fi
 done
+
